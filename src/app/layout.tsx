5612c7faf006b30c961a636e0fafc7a7db51a455
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-06-22 14:05:57 +0000

    insted of bg ..jpg use bg.mp4 video

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
 import { Toaster } from "@/components/ui/toaster";
-import Image from 'next/image';
 import { Poppins, DM_Sans } from 'next/font/google'
 import { cn } from '@/lib/utils';
 
@@ -35,14 +34,15 @@ export default function RootLayout({
     <html lang="en" className={cn("scroll-smooth", poppins.variable, dmSans.variable)}>
       <body className="font-body antialiased bg-black text-foreground">
         <div className="fixed inset-0 z-[-2]">
-            <Image
-            src="/bg.jpg"
-            alt="Background"
-            fill
-            sizes="100vw"
-            className="object-cover"
-            priority
-            />
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="w-full h-full object-cover"
+            >
+              <source src="/bg.mp4" type="video/mp4" />
+            </video>
             <div className="absolute inset-0 bg-black/60" />
         </div>
         <div className="relative z-0 flex flex-col min-h-screen">
