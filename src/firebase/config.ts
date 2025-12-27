e7974ce06f358faa34db14d797f46767fb881ff6
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-12-27 18:18:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "esystemlk-redesign",
+  "appId": "1:262221120894:web:bedf1a7fb15424555d5f2f",
+  "apiKey": "AIzaSyCk9DFbyUvx3ApZJBC5R0TeBbYuRR91iNI",
+  "authDomain": "esystemlk-redesign.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "262221120894"
+};
