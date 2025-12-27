2ba603f02bbbdb603b3e2b97908aea7e35bd9641
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-12-27 17:45:07 +0000

    also make home button on nav bar'[

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,6 +18,7 @@ const Navbar = () => {
   }, []);
 
   const navLinks = [
+    { name: "Home", href: "/" },
     { name: "Services", href: "/services" },
     { name: "Pricing", href: "/pricing" },
     { name: "Portfolio", href: "/portfolio" },
