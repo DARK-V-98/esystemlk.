5beb647dd61fd8e803ff03d84cadd10e45d733a9
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-12-27 23:04:48 +0000

    Error: Element type is invalid. Received a promise that resolves to: und

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,4 @@
+
 import { ContactForm } from "./contact-form";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Mail, MessageCircle, Phone } from "lucide-react";
