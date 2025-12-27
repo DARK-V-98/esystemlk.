1a964bb965e9dbb356adffcfe6256d1aa8c8d6bf
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-12-27 22:47:30 +0000

    bfore the dns lookup update this erorr dint came what is the reson with

--- src/app/api/dns-lookup/route.ts
+++ src/app/api/dns-lookup/route.ts
@@ -1,6 +1,6 @@
 
 import { NextResponse } from 'next/server';
-import { promises as dns, DNSServer } from 'dns';
+import { promises as dns } from 'dns';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
