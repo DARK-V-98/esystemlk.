31d13cf7756e7a8826d68deae1d0b5f656bef49e
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-12-28 15:29:16 +0000

    https://x.com/esystemlk add this link remove twitter mark adnd add x mar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,6 @@ import {
   Phone, 
   MapPin, 
   Facebook, 
-  Twitter, 
   Linkedin, 
   Instagram,
   Heart,
@@ -15,6 +14,28 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
+
+const XIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+    />
+  </svg>
+);
+
+
+const socialLinks = [
+  { Icon: Facebook, href: "https://www.facebook.com/esystemlk" },
+  { Icon: XIcon, href: "https://x.com/esystemlk" },
+  { Icon: Linkedin, href: "https://www.linkedin.com/company/esystemlk" },
+  { Icon: Instagram, href: "https://www.instagram.com/esystemlk" }
+];
+
 
 const Footer = () => {
   const scrollToTop = () => {
@@ -42,10 +63,12 @@ const Footer = () => {
             </p>
             {/* Social Links */}
             <div className="flex gap-3">
-              {[Facebook, Twitter, Linkedin, Instagram].map((Icon, index) => (
+              {socialLinks.map(({ Icon, href }, index) => (
                 <a
                   key={index}
-                  href="#"
+                  href={href}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="w-10 h-10 rounded-full bg-accent-foreground/5 border border-accent-foreground/10 flex items-center justify-center hover:bg-primary hover:border-primary hover:text-primary-foreground transition-all duration-300 group"
                 >
                   <Icon className="w-5 h-5" />
